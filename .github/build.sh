4f862808079842ca66c83b0741d4ffb19bc877bb
Harry S <harry.seeber@gusto.com>
2021-02-26 19:28:30 -0700

    use go pkg, use fq go path

--- .github/build.sh
+++ .github/build.sh
@@ -4,9 +4,9 @@ set -xe
 
 export GOARCH="amd64"
 export GOOS="darwin"
-go build -o committer.amd64 committer.go
+/usr/local/go/bin/go build -o committer.amd64 committer.go
 
 export GOARCH="arm64"
-go build -o committer.arm64 committer.go
+/usr/local/go/bin/go build -o committer.arm64 committer.go
 
-lipo committer.amd64 committer.arm64 -create -output committer
+/usr/bin/lipo committer.amd64 committer.arm64 -create -output committer
